be3730adfe166734f0a8ba0ff8e76609df6986f3
Alfonso-Rangel <luisrangel187@hotmail.com>
2024-04-23 12:24:47 -0600

    Se agrega la funcionalidad de nivel de agua empleando un visualizador 7 segmentos.

---
+++ Nivel-Agua/README.md
@@ -0,0 +1,18 @@
+# Funcionalidad Nivel del Agua
+### Objetivo 
+Desarrollar un panel que controle el nivel de agua mediante al menos un botón 
+
+### Lista de Archivos
+1. *CMakeLists.txt* :: Necesario para la construcción del proyecto.
+2. *nivel_agua.c* :: Código de la funcionalidad principal
+
+### Materiales Requeridos
+Una lista de materiales necesarios para visualizar el módulo del proyecto.
+| Material |  Cantidad  |
+|:-----|:--------:|
+| Raspberry Pico W | 1 | 
+| 7 segmentos |  1  |
+| Resistencia 85 ohm | 7 | 
+| Botón | 1 |
+| Cable | 10 |
+
--- Nivel-Agua/nivel_agua.c
+++ Nivel-Agua/nivel_agua.c
@@ -1,91 +1,98 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-#define FIRST_GPIO 2
-#define BUTTON_GPIO (FIRST_GPIO+7)
-
-
-// Este array convierte un número del 0 al 9 en un patrón de bits para enviar a los GPIOs
-
-// Ánodo común
-int bits[10] = {
-    0xC0,  // 0
-    0xF9,  // 1
-    0xA4,  // 2
-    0xB0,  // 3
-    0x99,  // 4
-    0x92,  // 5
-    0x82,  // 6
-    0xF8,  // 7
-    0x80,  // 8
-    0x98   // 9
-};
-
-// Cátodo común
 /*
-int bits[10] = {
-        0x3f,  // 0
-        0x06,  // 1
-        0x5b,  // 2
-        0x4f,  // 3
-        0x66,  // 4
-        0x6d,  // 5
-        0x7d,  // 6
-        0x07,  // 7
-        0x7f,  // 8
-        0x67   // 9
-};
+  Our 7 Segment display has pins as follows:
+
+  --A--
+  F   B
+  --G--
+  E   C
+  --D--
+
+  By default we are allocating GPIO 2 to segment A, 3 to B etc.
+  So, connect GPIO 2 to pin A on the 7 segment LED display etc. Don't forget
+  the appropriate resistors, best to use one for each segment!
+
+  Connect button so that pressing the switch connects the GPIO 9 (default) to
+  ground (pull down)
 */
 
+#define FIRST_GPIO 2
+#define BUTTON_GPIO (FIRST_GPIO+7)
+#define LENGTH 4
+
+// This array converts a number 0-9 to a bit pattern to send to the GPIOs
+
+int bits[LENGTH] = {
+       //0x40 - A
+       //0x20 - B
+       //0x10 - C
+       //0x08 - D
+       //0x04 - E
+       //0x02 - F
+       //0x01 - G
+       0x00,
+       0x08, // 0000 1000
+       0x48, // 0100 1000
+       0x49 // 0100 1001
+};
+
+/// \tag::hello_gpio[]
 int main() {
     stdio_init_all();
-    
-    // Configurar pines GPIO como salida
+    printf("Hello, 7segment - press button to count down!\n");
+
+    // We could use gpio_set_dir_out_masked() here
     for (int gpio = FIRST_GPIO; gpio < FIRST_GPIO + 7; gpio++) {
         gpio_init(gpio);
         gpio_set_dir(gpio, GPIO_OUT);
+        // Our bitmap above has a bit set where we need an LED on, BUT, we are pulling low to light
+        // so invert our output
+        gpio_set_outover(gpio, GPIO_OVERRIDE_INVERT);
     }
 
-    // Configurar pin del botón como entrada con pull-up
     gpio_init(BUTTON_GPIO);
     gpio_set_dir(BUTTON_GPIO, GPIO_IN);
+    // We are using the button to pull down to 0v when pressed, so ensure that when
+    // unpressed, it uses internal pull ups. Otherwise when unpressed, the input will
+    // be floating.
     gpio_pull_up(BUTTON_GPIO);
 
-    bool count_down = false; // Estado de la cuenta regresiva
     int val = 0;
+    bool button_pressed = false;
     while (true) {
-        // Si se presiona el botón, cambiar el estado de la cuenta regresiva
+        // Count upwards or downwards depending on button input
+        // We are pulling down on switch active, so invert the get to make
+        // a press count downwards
         if (!gpio_get(BUTTON_GPIO)) {
-            count_down = !count_down;
-            sleep_ms(250); // Esperar un momento para evitar la detección múltiple del botón
-        }
-
-        // Si estamos en modo de cuenta regresiva, contar hacia abajo
-        if (count_down) {
-            // Mostrar el valor actual
-            int32_t mask = bits[val];
-            for (int i = 0; i < 7; i++) {
-                if (mask & (1 << i)) {
-                    gpio_put(FIRST_GPIO + i, 1);
+            if (!button_pressed) {
+                button_pressed = true;
+                if (val == LENGTH - 1) {
+                    val = 0;
                 } else {
-                    gpio_put(FIRST_GPIO + i, 0);
+                    val++;
                 }
             }
-
-            // Decrementar el valor y ajustarlo si es necesario
-            val--;
-            if (val < 0) {
-                val = 9;
-            }
-
-            sleep_ms(1000); // Esperar un segundo antes de mostrar el siguiente valor
         } else {
-            // Si no estamos en modo de cuenta regresiva, mostrar 0
-            for (int i = 0; i < 7; i++) {
-                gpio_put(FIRST_GPIO + i, 0);
-            }
-            val = 0; // Reiniciar el valor
+            button_pressed = false;
         }
+
+        // We are starting with GPIO 2, our bitmap starts at bit 0 so shift to start at 2.
+        int32_t mask = bits[val] << FIRST_GPIO;
+
+        // Set all our GPIOs in one go!
+        // If something else is using GPIO, we might want to use gpio_put_masked()
+        gpio_set_mask(mask);
+        sleep_ms(150);
+        gpio_clr_mask(mask);
     }
 }
+/// \end::hello_gpio[]
