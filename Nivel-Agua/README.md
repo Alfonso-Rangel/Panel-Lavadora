0439b4f5f7700036c8b75aed78a52602eb80017e
Alfonso-Rangel <luisrangel187@hotmail.com>
2024-04-23 20:31:09 -0600

    Se completa la visualización del nivel con un led de 10 segmentos y dos botones para seleccionar el nivel.

--- Nivel-Agua/README.md
+++ Nivel-Agua/README.md
@@ -1,18 +1,28 @@
 # Funcionalidad Nivel del Agua
 ### Objetivo 
-Desarrollar un panel que controle el nivel de agua mediante al menos un botón 
+Desarrollar un panel que controle el nivel carga para asignar el nivel del agua de la lavadora.
+Este panel cuenta con una barra de leds de 10 segmentos y dos botones para bajar y subir el nivel.
+Hay 4 niveles en total. 
 
 ### Lista de Archivos
 1. *CMakeLists.txt* :: Necesario para la construcción del proyecto.
-2. *nivel_agua.c* :: Código de la funcionalidad principal
+2. *nivel_agua.c* :: Código de la funcionalidad de nivel de carga.
 
 ### Materiales Requeridos
 Una lista de materiales necesarios para visualizar el módulo del proyecto.
 | Material |  Cantidad  |
 |:-----|:--------:|
 | Raspberry Pico W | 1 | 
-| 7 segmentos |  1  |
-| Resistencia 85 ohm | 7 | 
-| Botón | 1 |
-| Cable | 10 |
+| Barra 10 segmentos |  1  |
+| Resistencia 85 ohm | 14 | 
+| Botón | 2 |
+| Cable tipo Dupont| 26 |
 
+
+### Reporte de Tareas | 23/04/2024
+Estas tareas tienen como objetivo ayudarme a estudiar el funcionamiento de la Raspberry para lograr el objetivo de este módulo del panel.
+- [✓] Encender todos los leds con un botón para comprobar que las conexiones sean correctas. 
+- [✓] Usar el botón para encender los leds uno por uno, de forma secuencial.
+- [✓] Conectar un segundo botón y encender los leds en sentido contrario al primer botón.
+- [✓] Encender varios leds a la vez, pues de 10 leds sólo se hacen hay 4 niveles.
+- [ ] Crear una función que devuelva el nivel de carga actualmente seleccionado.
--- Nivel-Agua/nivel_agua.c
+++ Nivel-Agua/nivel_agua.c
@@ -26,32 +26,44 @@
 */
 
 #define FIRST_GPIO 2
-#define BUTTON_GPIO (FIRST_GPIO+7)
-#define LENGTH 4
+#define BUTTON_GPIO_UP (FIRST_GPIO + 11)
+#define BUTTON_GPIO_DOWN (FIRST_GPIO + 10)
+#define LENGTH 5
 
 // This array converts a number 0-9 to a bit pattern to send to the GPIOs
-
+/*
 int bits[LENGTH] = {
-       //0x40 - A
-       //0x20 - B
-       //0x10 - C
-       //0x08 - D
-       //0x04 - E
-       //0x02 - F
-       //0x01 - G
-       0x00,
-       0x08, // 0000 1000
-       0x48, // 0100 1000
-       0x49 // 0100 1001
+       0b0000000000, 
+       0b0000000001, // 1 A
+       0b0000000010, // 2 V
+       0b0000000100, // 3 V
+       0b0010000000, // 4 V
+       0b0000010000, // 5 V
+       0b0000100000, // 6 N
+       0b0001000000, // 7 N
+       0b0000001000, // 8 N 
+       0b0100000000, // 9 R
+       0b1000000000, // 10 R
+       0b1111111111
+
 };
+*/
+int bits[LENGTH] = {
+       0b0000000000, // Sin carga 
+       0b0000000001, // Carga baja
+       0b0010010111, // Carga media
+       0b0011111111, // Carga moderada
+       0b1111111111 // Capacidad máxima
+};
+
+
 
 /// \tag::hello_gpio[]
 int main() {
     stdio_init_all();
-    printf("Hello, 7segment - press button to count down!\n");
 
     // We could use gpio_set_dir_out_masked() here
-    for (int gpio = FIRST_GPIO; gpio < FIRST_GPIO + 7; gpio++) {
+    for (int gpio = FIRST_GPIO; gpio < FIRST_GPIO + 10; gpio++) {
         gpio_init(gpio);
         gpio_set_dir(gpio, GPIO_OUT);
         // Our bitmap above has a bit set where we need an LED on, BUT, we are pulling low to light
@@ -59,12 +71,15 @@ int main() {
         gpio_set_outover(gpio, GPIO_OVERRIDE_INVERT);
     }
 
-    gpio_init(BUTTON_GPIO);
-    gpio_set_dir(BUTTON_GPIO, GPIO_IN);
+    gpio_init(BUTTON_GPIO_UP);
+    gpio_init(BUTTON_GPIO_DOWN);
+    gpio_set_dir(BUTTON_GPIO_UP, GPIO_IN);
+    gpio_set_dir(BUTTON_GPIO_DOWN, GPIO_IN);
     // We are using the button to pull down to 0v when pressed, so ensure that when
     // unpressed, it uses internal pull ups. Otherwise when unpressed, the input will
     // be floating.
-    gpio_pull_up(BUTTON_GPIO);
+    gpio_pull_up(BUTTON_GPIO_UP);
+    gpio_pull_up(BUTTON_GPIO_DOWN);
 
     int val = 0;
     bool button_pressed = false;
@@ -72,13 +87,22 @@ int main() {
         // Count upwards or downwards depending on button input
         // We are pulling down on switch active, so invert the get to make
         // a press count downwards
-        if (!gpio_get(BUTTON_GPIO)) {
+        if (!gpio_get(BUTTON_GPIO_UP)) {
             if (!button_pressed) {
                 button_pressed = true;
                 if (val == LENGTH - 1) {
                     val = 0;
                 } else {
                     val++;
+                }
+            }
+        } else if (!gpio_get(BUTTON_GPIO_DOWN)) {
+            if (!button_pressed) {
+                button_pressed = true;
+                if (val == 0) {
+                    val = LENGTH - 1;
+                } else {
+                    val--;
                 }
             }
         } else {
