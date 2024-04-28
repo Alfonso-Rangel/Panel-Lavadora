02b3b0f8356a5ab87fe3b44e0aef041b033edd97
Andre-RG <andrereyesgarcia1@gmail.com>
2024-04-27 22:55:59 -0600

    Archivo de prueba

---
+++ Temporizador/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "test.h"
+
+int main() {
+  int pins[] = {
+    PIN_A,
+    PIN_B,
+    PIN_C,
+    PIN_D,
+    PIN_E,
+    PIN_F,
+    PIN_G,
+    D1,
+    D2,
+    D3,
+    D4
+  };
+  timer_construct(pins);
+}
