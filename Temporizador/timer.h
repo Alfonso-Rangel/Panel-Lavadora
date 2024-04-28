9b2c27e89363034b2279db63bc3537c826631d88
Andre-RG <andrereyesgarcia1@gmail.com>
2024-04-27 22:54:13 -0600

    Prototipos de las funciones timer

---
+++ Temporizador/timer.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define PINS_SIZE 11
+
+/* @brief*/
+static uint32_t pins[PINS_SIZE];
+
+/* @brief*/
+static uint32_t timer_mask;
+
+/* @brief*/
+static uint32_t bits[] = {
+  0x3f,
+  0x06,
+  0x5b,
+  0x4f,
+  0x66,
+  0x6d,
+  0x7d,
+  0x07,
+  0x7f,
+  0x67 
+};
+
+/* @brief*/
+static uint32_t const delay = 6;
+
+void timer_construct(const int*);
+void timer_init();
