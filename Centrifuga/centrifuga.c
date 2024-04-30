2c6885fc1618e3d9a31cdf8c16b9b0236ae24cf1
Valeria <66036399+valepcm@users.noreply.github.com>
2024-04-30 09:24:48 -0600

    Update centrifuga.c

--- Centrifuga/centrifuga.c
+++ Centrifuga/centrifuga.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+
+#define FIRST_GPIO 2
+#define BUTTON_GPIO (FIRST_GPIO+7)
+
+// This array converts a number 0-9 to a bit pattern to send to the GPIOs
+int bits[10] = {
+        0x3f,  // 0
+        0x06,  // 1
+        0x5b,  // 2
+        0x4f,  // 3
+        0x66,  // 4
+        0x6d,  // 5
+        0x7d,  // 6
+        0x07,  // 7
+        0x7f,  // 8
+        0x67   // 9
+
+};
+
+int main() {
+    stdio_init_all();
+    printf("Centrifugado - Presiona el boton para empezar o reiniciar\n");
